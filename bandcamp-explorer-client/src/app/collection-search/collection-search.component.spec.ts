0acfc0456ed6c6e728bec25fbc7f9804efebabc3
Benjamin Kalbitz <bkalbitz@googlemail.com>
2025-06-26 10:15:03 +0200

    FE test for collection search component

--- bandcamp-explorer-client/src/app/collection-search/collection-search.component.spec.ts
+++ bandcamp-explorer-client/src/app/collection-search/collection-search.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CollectionSearchComponent } from './collection-search.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CollectionSearchComponent', () => {
   let component: CollectionSearchComponent;
@@ -14,10 +16,31 @@ describe('CollectionSearchComponent', () => {
 
     fixture = TestBed.createComponent(CollectionSearchComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('has headline', () => {
+    const h1 = fixture.debugElement.query(By.css('h1'));
+    expect(h1).toBeTruthy();
+    expect(h1.nativeElement.innerText).toBe('Search your bandcamp collection');
+  });
+
+  it('has disabled search button', () => {
+    expect(component.buttonDisalbed).toBeTruthy();
+  });
+
+  it('search button also disabled with invalid BC collection URL', () => {
+    component.collectioName = 'test';
+    expect(component.buttonDisalbed).toBeTruthy();
+  });
+
+  it('search button  enabled with BC collection URL', () => {
+    component.collectioName = 'https://bandcamp.com/my-collection-name';
+    expect(component.buttonDisalbed).toBeFalsy();
   });
 });
--- bandcamp-explorer-client/src/app/collection-search/collection-search.component.ts
+++ bandcamp-explorer-client/src/app/collection-search/collection-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
@@ -11,21 +11,32 @@ import { NavigationDataService } from '../navigation-data.service';
   templateUrl: './collection-search.component.html',
   styleUrl: './collection-search.component.css'
 })
-export class CollectionSearchComponent implements OnInit {
-  collectioName: string = ''
+export class CollectionSearchComponent implements OnInit{
+  readonly  validUrl = 'https://bandcamp.com/';
+  _collectioName: string = ''
+  buttonDisalbed = true;
   constructor(private router: Router,
     private navigationService: NavigationDataService) {
   }
+
   ngOnInit(): void {
     const previewSearch = this.navigationService.getCurrentState().data;
     this.collectioName = previewSearch ? previewSearch : '';
   }
+
+  get collectioName() {
+    return this._collectioName;
+  }
+
+  set collectioName(_collectioName: string) {
+    this.buttonDisalbed = !_collectioName?.startsWith(this.validUrl);
+    this._collectioName = _collectioName;
+  }
   
   search() {
-    const cut = 'https://bandcamp.com/'
-    if(this.collectioName.startsWith(cut)) {
+    if(this.collectioName.startsWith(this.validUrl)) {
       this.navigationService.registerState(this.collectioName);
-      const search = this.collectioName.substring(cut.length);
+      const search = this.collectioName.substring(this.validUrl.length);
       this.router.navigate(['collection', search]);
     }
     
