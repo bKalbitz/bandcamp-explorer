22a28af08aa44af272e2d33535fd97686924e266
Benjamin Kalbitz <bkalbitz@googlemail.com>
2025-08-03 20:32:51 +0200

    Complete FE tests for collection intersection data display

--- bandcamp-explorer-client/src/app/collection-intersection/collection-intersection.component.spec.ts
+++ bandcamp-explorer-client/src/app/collection-intersection/collection-intersection.component.spec.ts
@@ -83,8 +83,12 @@ describe('CollectionIntersectionComponent', () => {
   let component: CollectionIntersectionComponent;
   let fixture: ComponentFixture<CollectionIntersectionComponent>;
 
-  beforeAll(async () => {
-    const service = jasmine.createSpyObj('CollectionIntersectionService', {'loadIntersection':dataRequest, 'getIntersectionLoadingState':requestComplete, 'getIntersectionResult':collectionsData});
+  beforeEach(async () => {
+    let service = jasmine.createSpyObj('CollectionIntersectionService', ['loadIntersection', 'getIntersectionLoadingState', 'getIntersectionResult']);
+    service.loadIntersection.and.returnValue(Promise.resolve(dataRequest));
+    service.getIntersectionLoadingState.and.returnValue(Promise.resolve(requestComplete));
+    service.getIntersectionResult.and.returnValue(Promise.resolve(collectionsData));
+
     await TestBed.configureTestingModule({
       imports: [CollectionIntersectionComponent],
       providers: [provideRouter(routes), {
@@ -121,7 +125,7 @@ describe('CollectionIntersectionComponent', () => {
     expect(h1.nativeElement.innerText).toBe('Collection Intersection');
   });
 
-  it('has three entries', async () => {
+  it('has three entries in data', async () => {
      while(component.loadingState < 100) {
       await new Promise(resolve => setTimeout(resolve, 200));
     }
@@ -129,4 +133,56 @@ describe('CollectionIntersectionComponent', () => {
     expect(component.dataReady).toBeTruthy();
     expect(component.entries?.length).toBe(3);
   });
+
+
+  it('should display three album entries when data is ready', async () => {
+    while(component.loadingState < 100) {
+      await new Promise(resolve => setTimeout(resolve, 200));
+    }
+    await fixture.whenStable();
+    fixture.detectChanges();
+    
+    const albumEntries = fixture.debugElement.queryAll(By.css('.bc-ce-intersection-entry'));
+    expect(albumEntries.length).toBe(3);
+
+    let albumInfo = albumEntries[0].queryAll(By.css('.bc-ce-intersection-entry-album p'));
+    expect(albumInfo[0].nativeElement.textContent).toBe('album-1');
+    expect(albumInfo[1].nativeElement.textContent).toBe('by artist-1');
+
+    let albumLink = albumEntries[0].query(By.css('a'));
+    expect(albumLink.nativeElement.href).toBe('https://arist-1.bandcamp.com/album/album-1');
+
+    let inCollection = albumEntries[0].queryAll(By.css('div.bc-ce-intersection-collections a'));
+    expect(inCollection.length).toBe(3);
+    expect(inCollection[0].nativeElement.textContent).toBe('collection-1');
+    expect(inCollection[1].nativeElement.textContent).toBe('collection-2');
+    expect(inCollection[2].nativeElement.textContent).toBe('collection-3');
+
+    albumInfo = albumEntries[1].queryAll(By.css('.bc-ce-intersection-entry-album p'));
+    expect(albumInfo[0].nativeElement.textContent).toBe('album-2');
+    expect(albumInfo[1].nativeElement.textContent).toBe('by artist-1');
+
+    albumLink = albumEntries[1].query(By.css('a'));
+    expect(albumLink.nativeElement.href).toBe('https://arist-1.bandcamp.com/album/album-2');
+
+    inCollection = albumEntries[1].queryAll(By.css('div.bc-ce-intersection-collections a'));
+    expect(inCollection.length).toBe(2);
+    expect(inCollection[0].nativeElement.textContent).toBe('collection-1');
+    expect(inCollection[1].nativeElement.textContent).toBe('collection-3');
+
+
+    albumInfo = albumEntries[2].queryAll(By.css('.bc-ce-intersection-entry-album p'));
+    expect(albumInfo[0].nativeElement.textContent).toBe('album-3');
+    expect(albumInfo[1].nativeElement.textContent).toBe('by artist-2');
+
+    albumLink = albumEntries[2].query(By.css('a'));
+    expect(albumLink.nativeElement.href).toBe('https://arist-2.bandcamp.com/album/album-3');
+
+    inCollection = albumEntries[2].queryAll(By.css('div.bc-ce-intersection-collections a'));
+    expect(inCollection.length).toBe(2);
+    expect(inCollection[0].nativeElement.textContent).toBe('collection-2');
+    expect(inCollection[1].nativeElement.textContent).toBe('collection-3');
+
+  });
+
 });
